e0311806cd4da079df7ab10b4f6ecb9ea90a2ee4
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2013-11-17 20:59:02 +0000

    new: overcom

---
+++ trunk/prj.sandbox/overcom/src/main.cpp
@@ -0,0 +1,25 @@
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+void estimate_overcom( format( data.c_str(), i ).c_str() )
+{
+  ;
+}
+
+
+int main( int argc, char* argv[] )
+{
+  string exe  = argv[0];
+  string data = exe + "/../../../testdata/overcom/overcom01";
+
+  for (int i=1; i<100; i++)
+    estimate_overcom( format( data.c_str(), i ).c_str() );
+
+	return 0;
+}
