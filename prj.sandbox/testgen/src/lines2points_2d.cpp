65cc84d6d03536d47dd4bf9baf7feeb4733552b8
Vassili.Postnikov <Vassili.Postnikov@gmail.com>
2014-01-16 22:36:48 +0000

    new: testgen -- апликация для генерации синтетических тестов

---
+++ prj.sandbox/testgen/src/lines2points_2d.cpp
@@ -0,0 +1,6 @@
+// 2d lines with vanish points clusters generator
+#include "precomp.h"
+
+void  testgen_lines2points_2d( string res_folder )
+{
+}
---
+++ prj.sandbox/testgen/src/points2lines_2d.cpp
@@ -0,0 +1,6 @@
+// 2d points along lines clusters generator
+#include "precomp.h"
+
+void  testgen_points2lines_2d( string res_folder )
+{
+}
