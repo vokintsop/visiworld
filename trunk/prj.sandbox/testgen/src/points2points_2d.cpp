4ccd9b6b4329a5f1a30a6c54c3c7cf901701296c
krohina0d@gmail.com <krohina0d@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2014-01-17 18:20:23 +0000

    Добавлен код для генерации points2points_2d и gen.h

--- trunk/prj.sandbox/testgen/src/points2points_2d.cpp
+++ trunk/prj.sandbox/testgen/src/points2points_2d.cpp
@@ -1,8 +1,10 @@
 // 2d points clusters generator
 #include "precomp.h"
+#include "gen.h"
 const int xMax = 1000;
 const int yMax = 1000;
 const int sigma = 20; // радиус кластера, сигма
+const int countPoints = 500; // количество точек в кластере
 
 void  testgen_points2points_2d( string res_folder )
 {
@@ -18,15 +20,15 @@ void  testgen_points2points_2d( string res_folder )
     }
 
     ////////// generate points around clusters
-    ////////for (double num_points_f =1; num_points_f < 100000; num_points_f *=1.2 )
-    ////////{
-    ////////  int num_points = int( num_points_f );
-    ////////  for (int i=0; i<num_points; i++)
-    ////////  {
-
-    ////////  }
-
-    ////////}
+    vector<Point> p;// все точки в кластере
+    for (int i_cluster = 0; i_cluster < num_clusters; i_cluster++)
+    {
+      for (int i = 0; i < countPoints; ++i)
+      {
+        p.push_back(gen_point(clusters[i_cluster].first, clusters[i_cluster].second));
+      }
+    }
+    random_shuffle(p.begin(), p.end());
 
     string test_name = res_folder + format( "clu%.03d", num_clusters );
     Mat1b res( 1000, 1000, 255 );
@@ -35,6 +37,12 @@ void  testgen_points2points_2d( string res_folder )
     {
       circle( res, clusters[i].first, clusters[i].second, Scalar(0,0,0,0), 2 );
       out <<  clusters[i].first.x << "\t" <<  clusters[i].first.y  << "\t" <<  clusters[i].second << endl;
+    }
+    out << p.size() << endl; // количество точек
+    for (int i = 0; i < p.size(); ++i)
+    {
+      out << p[i].x << " " << p[i].y << endl;
+      circle( res, p[i], 2, Scalar(0, 0, 0), 2); //сами точки
     }
     imwrite( test_name+".png", res );
   }
