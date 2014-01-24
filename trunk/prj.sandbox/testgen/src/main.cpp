8c6d268da06c1b291947b184ae7863c4896ffe43
krohina0d@gmail.com <krohina0d@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2014-01-24 06:59:59 +0000

    Добавлен тест Гаусса - выводится распределение

--- trunk/prj.sandbox/testgen/src/main.cpp
+++ trunk/prj.sandbox/testgen/src/main.cpp
@@ -1,5 +1,6 @@
 // synt test generator
 #include "precomp.h"
+#include "gen.h"
 const int xMax = 1000;
 const int yMax = 1000;
 const int sigma = 20; // радиус кластера, сигма
@@ -8,11 +9,31 @@ void  testgen_points2points_2d( string res_folder );
 void  testgen_points2lines_2d( string res_folder );
 
 
+void testGauss()
+{
+  Mat1b img(yMax, xMax);
+  img = 0;
+
+  vector<int> X(xMax, 0);
+  for(int i = 0; i < X.size(); ++i)
+  {
+     int x = Gauss_(xMax / 2, sigma);
+     if (x >= 0 && x < xMax)
+     {
+      X[x] += 1;
+      img(X[x], x) = 255;
+     }
+  }
+  imshow("img", img);
+  cvWaitKey(0);
+}
+
 int main( int argc, char* argv[] )
 {
-  string exe = argv[0];
-  testgen_points2points_2d( exe +  "/../../../testdata/testgen/points2points_2d/" );
-  testgen_points2lines_2d( exe +  "/../../../testdata/testgen/points2lines_2d/" );
+  testGauss();
+  //string exe = argv[0];
+  //testgen_points2points_2d( exe +  "/../../../testdata/testgen/points2points_2d/" );
+  //testgen_points2lines_2d( exe +  "/../../../testdata/testgen/points2lines_2d/" );
   //testgen_lines2points_2d( exe +  "/../../../testdata/testgen/lines2points_2d/" );
 
  
