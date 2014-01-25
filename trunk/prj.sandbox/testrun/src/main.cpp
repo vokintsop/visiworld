5a99d4d2bc28a185d3e6c80ef18a005f0ebf9d42
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2014-01-25 17:53:35 +0000

    черновик прогона тестов. налажено по директориям с testgen, points2points_2d и подобные подняты на уровень выше

---
+++ trunk/prj.sandbox/testrun/src/main.cpp
@@ -0,0 +1,17 @@
+// synt test generator
+#include "precomp.h"
+
+void  testrun_points2points_2d( const string& input_folder,  const string& output_folder ); // фолдеры могут совпадать
+//void  testrun_points2lines_2d( const string& input_folder,  const string& output_folder );
+
+int main( int argc, char* argv[] )
+{
+  string exe = argv[0];
+  string testdata = exe + "/../../../testdata/";
+  testrun_points2points_2d( testdata + "points2points_2d/clu%03d" ,   testdata + "points2points_2d/clu%03d" );
+  //testrun_points2lines_2d( exe +  "/../../../testdata/points2lines_2d/" );
+  //testrun_lines2points_2d( exe +  "/../../../testdata/lines2points_2d/" );
+
+ 
+	return 0;
+}
