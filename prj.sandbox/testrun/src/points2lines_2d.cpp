e0216d874cd7d821164fad48c1ac1f43e5ff2b91
krohina0d <krohina0d@gmail.com>
2014-02-24 08:08:50 +0000

    Вынесла hcoords выше - Hcoords.h - структура для однородных координат

--- prj.sandbox/testrun/src/points2lines_2d.cpp
+++ prj.sandbox/testrun/src/points2lines_2d.cpp
@@ -13,56 +13,6 @@ Point3d& normalize( Point3d& v ) // set length == 1
   return v;
 }
 
-struct HCoords
-{
-  int width, height, depth; // image bitmap size and camera angle
-  HCoords(
-  int width, // tmp
-  int height, // tmp
-  int depth = 0 // tmp
-  ): width(width), height(height), depth(depth)
-  {
-    if (depth == 0)
-      depth = width/2; // предположим, что 90 градусов по горизонтали
-  }
-
-  Point3d convert( // << результат = нормализованный вектор
-  const Point& p // на входе точка в исходных координатах изображения
-                )
-  {
-    if (depth == 0)
-      depth = width/2; // 90 градусов по умолчанию
-    Point3d res = Point3d( p.x - width/2, p.y - height/2, depth );
-    return normalize( res );
-  }
-
-  void hline2points( 
-    const Point3d& hline, // линия в однородных координатах
-    Point& pt1, Point& pt2 // линия как две точки на краях битмапа
-    )
-  {
-    // hline == A, B, C,  Ax+By+Cz = 0; z = depth;
-    double A = hline.x;
-    double B = hline.y;
-    double CC = -hline.z*depth;
-    Point2d h1, h2;
-    if (abs(A) > abs(B))
-    {
-      h1.y = -height/2.; h1.x = (CC - B*h1.y) / A;
-      h2.y =  height/2.; h2.x = (CC - B*h2.y) / A;
-    }
-    else
-    {
-      h1.x = -width/2.; h1.y = (CC - A*h1.x) / B;
-      h2.x =  width/2.; h2.y = (CC - A*h2.x) / B;
-    }
-    pt1.x = h1.x + width/2; 
-    pt2.x = h2.x + width/2; 
-    pt1.y = h1.y + height/2; 
-    pt2.y = h2.y + height/2; 
-  }
-};
-
 struct FoundLine
 {
   Point pt1, pt2; // в координатах битмапа
