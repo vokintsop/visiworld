5a16b7443325bb215135bad0f830ea1c96673bee
Vassili.Postnikov <Vassili.Postnikov@gmail.com>
2014-03-30 22:35:29 +0000

    раскидал основные части проекта YorkUrbanDB по плэйсхолдерам-пустышкам

--- prj.sandbox/yorkurbandb/src/main.cpp
+++ prj.sandbox/yorkurbandb/src/main.cpp
@@ -22,117 +22,69 @@
 #include <cassert>
 
 #include "cover_net.h"
+#include "imagerecord.h"
 
 using namespace std;
 using namespace cv;
 
-bool read_samples( const char* path )
-{
-  ifstream in( path );
 
-  if (!in.is_open())
-  {
-    cout << "Can't open " << path << endl;
-    return false;
-  }
 
-// todo astdcall
 
-/*
-  while (in.good())
-  {
-    Mat mat( 16, 16, CV_8UC1 );
-    for ( int y=0; y<16; y++ )
-    {
-      for ( int x=0; x<16; x++ )
-      {
-        double val=0.5;
-        in >> val;
-        mat.at<uchar>( y, x ) = cvRound( 255*val );
-      }
-    }
-    
-    int chr=-1;
-    for ( int c=0; c<10; c++ )
-    {
-      int cc = -1;
-      in >> cc;
-      if (cc == 1)
-        chr = c;
-      else
-        assert( cc == 0 );
-    }
-    string dummy;
-    getline( in, dummy ); // skip cr-lf
-
-    if (chr>=0 && chr <=9)
-      samples.push_back( make_pair( chr, mat ) );
-    else
-      break;
-  }
-*/
-  cout << samples.size() << " samples read from " << path << endl;;
-
-  return true;
-}
-
-class Metr1 // .........
-{
-public:
-  double computeDistance( const int& i1,  const int& i2 )  // индексы к samples[]
-  {
-    double dst=0;
-    Mat m1 = samples[i1].second;
-    Mat m2 = samples[i2].second;
-    for ( int y=0; y<16; y++ )
-    {
-      for ( int x=0; x<16; x++ )
-      {
-        if (m1.at<uchar>( y, x ) != m2.at<uchar>( y, x ) )
-          dst += 1;
-      }
-    }
-    return dst;
-  }
-};
-
-void explore_yourkurbandb()
-{
-
-  //Metr1 ruler;  CoverTree< int, Metr1 > tree( &ruler, 1000, 1 );
-  Metr2 ruler;  CoverTree< int, Metr2 > tree( &ruler, 1000, 1 );
-
-  for (int i=0; i< int( samples.size() ); i++)
-    tree.insert( i );
-
-  tree.reportStatistics( 0, 3 ); 
-
-}
 
 int main( int argc, char* argv[] )
 {
   string exe  = argv[0];
-  //string data = exe + "/../../../testdata/yourkurbandb";
-  string data = "d:/testdata/yourkurbandb";
-  if (!read_samples(data.c_str()))
+  //string folder = exe + "/../../../testdata/yorkurbandb";
+  string folder = "//testdata/yorkurbandb";
+  vector< ImageRecord > image_records; // данные по картинкам
+  if (!read_image_records( folder, image_records ) || image_records.size() < 1 )
     return -1;
 
-  explore_yourkurbandb();
+  for (int i=0; i<int( image_records.size() ); i++)
+    image_records[i].explore();
 
-  int key=-1;
-  for ( int frame =0; key != 27 && frame < int( samples.size() ); )
-  {
-    Mat matx;
-    resize( samples[frame].second, matx, Size(), 16., 16. );
+  make_report( image_records );
 
-    imshow( "sample", matx );
-    key = waitKey(50);
-    switch (key)
-    {
-    case 27: break;
-    default:
-      frame = (frame+1) % samples.size();
-    }
-  }
   return 0;
 }
+
+bool read_image_records( string& root, vector< ImageRecord >& image_records )
+{
+  // todo: fill image_records[] inputs
+  return true;
+}
+
+void ImageRecord::explore()
+{
+
+  ////Metr1 ruler;  CoverTree< int, Metr1 > tree( &ruler, 1000, 1 );
+  //Metr2 ruler;  CoverTree< int, Metr2 > tree( &ruler, 1000, 1 );
+
+  //for (int i=0; i< int( samples.size() ); i++)
+  //  tree.insert( i );
+
+  //tree.reportStatistics( 0, 3 ); 
+
+}
+
+
+//
+//class Metr1 // .........
+//{
+//public:
+//  double computeDistance( const int& i1,  const int& i2 )  // индексы к samples[]
+//  {
+//    double dst=0;
+//    Mat m1 = samples[i1].second;
+//    Mat m2 = samples[i2].second;
+//    for ( int y=0; y<16; y++ )
+//    {
+//      for ( int x=0; x<16; x++ )
+//      {
+//        if (m1.at<uchar>( y, x ) != m2.at<uchar>( y, x ) )
+//          dst += 1;
+//      }
+//    }
+//    return dst;
+//  }
+//};
---
+++ prj.sandbox/yorkurbandb/src/report.cpp
@@ -0,0 +1,7 @@
+// report.cpp
+#include "imagerecord.h"
+
+void make_report( std::vector< ImageRecord >& image_records )
+{
+  // todo
+}
