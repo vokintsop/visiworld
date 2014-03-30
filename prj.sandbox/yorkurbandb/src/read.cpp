8731b3e995796ff30ac57c328817d9492ee2719a
Vassili.Postnikov <Vassili.Postnikov@gmail.com>
2014-03-30 22:38:25 +0000

    york:: minor, дораскидал

--- prj.sandbox/yorkurbandb/src/read.cpp
+++ prj.sandbox/yorkurbandb/src/read.cpp
@@ -1,1 +1,13 @@
 // read.cpp
+
+#include "imagerecord.h"
+
+using namespace std;
+using namespace cv;
+
+bool read_image_records( std::string& root, std::vector< ImageRecord >& image_records )
+{
+  // todo: fill image_records[] inputs
+  return true;
+}
+
