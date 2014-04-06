6e0dbc045a47c51667b08df8da6fc32cd0feaaad
Vassili.Postnikov <Vassili.Postnikov@gmail.com>
2014-04-06 02:15:40 +0000

    добавлен подсчет точек-пересечений, попадающих в сферы дерева

--- prj.sandbox/yorkurbandb/src/explore.cpp
+++ prj.sandbox/yorkurbandb/src/explore.cpp
@@ -18,6 +18,8 @@ public:
 #if 1
     Point3d& p1 = upoints[cp1];
     Point3d& p2 = upoints[cp2];
+    assert( length(p1) < 1.001 );    assert( length(p1) > 0.999 );
+    assert( length(p2) < 1.001 );    assert( length(p2) > 0.999 );
     double cos = p1.ddot( p2 );
     return acos( max( -1., min( 1., cos ) ) );
 #else
@@ -69,7 +71,7 @@ void ImageRecord::explore()
   for (int i=0; i< int( hlines.size() ); i++)
     for (int j=0; j<i; j++ ) // рассмотрим пары 0 <= j < i < hlines.size() 
     {
-      Point3d i_j_intersection = hlines[i].cross( hlines[j] );
+      Point3d i_j_intersection = normalize( hlines[i].cross( hlines[j] ) );
       i_j_intersection = force_positive_direction( i_j_intersection );
       hlines_intersections.push_back( i_j_intersection );
     }
@@ -77,13 +79,44 @@ void ImageRecord::explore()
   UnionSpereAnglesRuler ruler( hlines_intersections );
 
   //CoverNet< int, UnionSpereAnglesRuler >  cover_net( &ruler, CV_PI, 0.05 ); 
-  CoverNet< int, UnionSpereAnglesRuler >  cover_net( &ruler, CV_PI*16, 0.001 ); 
-  for (int i=0; i< hlines_intersections.size(); i++)
+  CoverNet< int, UnionSpereAnglesRuler >  cover_net( &ruler, CV_PI, 0.001 ); 
+  for (int i=0; i< int( hlines_intersections.size() ); i++)
   {
     cover_net.insert(i);
   }
 
   cover_net.reportStatistics();
+
+  // осталось кластеризовать... пусть phi -- максимальное расхождение направлений точек схода
+  vector< int > true_weight;
+  cover_net.countTrueWeight( true_weight );
+  assert( true_weight.size() == cover_net.getSpheresCount() );
+  int nlevels = cover_net.getCountOfLevels(); // кол-во уровней в дереве
+  vector< vector< pair< int, int > > > // по каждому уровню [<кол-во покрываемых точек, номер сферы>]
+    density( nlevels ); ///, vector< pair< int, int > > ); 
+  for (int i=0; i< int( true_weight.size() ); i++) // i -- номер сферы
+  {
+    int level = cover_net.getSphereLevel(i);
+    int weight = true_weight[i];
+    density[level].push_back( make_pair( weight, i ) );
+  }
+  for (int i=0; i<cover_net.getCountOfLevels(); i++)
+    std::sort( density[i].rbegin(), density[i].rend() ); // на каждом уровне сортируем по числу покрываемых точек
+
+  bool print_next_level = true;
+  for (int i=0; i<cover_net.getCountOfLevels(); i++)
+  { // из любопытства выведем первые 10 точек -- сколько накрывают
+    if (print_next_level)
+      printf( "\nlevel= %2.d ", i );
+    print_next_level = false;
+    for ( int j=0; j < min( 10, int( density[i].size() ) ); j ++)
+    {
+      printf( "%5.d (%5.d) ", density[i][j].first, density[i][j].second );
+      print_next_level = true;
+    }
+  }
+
+  
 
   dbgPressAnyKey();
 
