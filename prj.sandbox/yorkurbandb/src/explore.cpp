fd0622f94256139c7d37eda0f9129800f965fbce
Vassili.Postnikov <Vassili.Postnikov@gmail.com>
2014-04-06 00:06:44 +0000

    yudb: explore.cpp: добавлено расчет пересечений и погружение в дерево

--- prj.sandbox/yorkurbandb/src/explore.cpp
+++ prj.sandbox/yorkurbandb/src/explore.cpp
@@ -1,18 +1,35 @@
 // explore.cpp
 
+#include "ocvutils/precomp.h"
 #include "imagerecord.h"
 
 
 using namespace std;
 using namespace cv;
 
-//#include "cover_net.h"
+#include "cover_net/cover_net.h"
 
+class UnionSpereAnglesRuler {
+public:
+  vector< Point3d >& upoints; // ссылка на внешний массив -- мн-во точек на единичной сфере
+  UnionSpereAnglesRuler( vector< Point3d >& upoints): upoints(upoints){};
+  double computeDistance(  int cp1,  int cp2 )
+  {
+#if 1
+    Point3d& p1 = upoints[cp1];
+    Point3d& p2 = upoints[cp2];
+    double cos = p1.ddot( p2 );
+    return acos( max( -1., min( 1., cos ) ) );
+#else
+    return upoints[cp1].ddot( upoints[cp2] );
+#endif
+  }
+};
 
 
 void ImageRecord::explore()
 {
-  
+
   {//////////// show segments
     string imagename = name + ".jpg";
     Mat mat = imread( imagename );
@@ -32,12 +49,14 @@ void ImageRecord::explore()
       hlines.push_back( hline );
   }
 
-  {//////////// show hlines
+  {//////////// show & print hlines
     string imagename = name + ".jpg";
     Mat mat = imread( imagename );
     for (int i=0; i< int(hlines.size()); i++)
     {
       Point p1,p2; 
+      //cout << "hline: " << hlines[i] << endl;
+      //cout << "length: " << length( hlines[i] ) << endl;
       hcoords.hline2points( hlines[i], p1, p2 );
       line( mat, p1, p2, Scalar( 200, 0, 200 ) );
     }
@@ -46,14 +65,29 @@ void ImageRecord::explore()
     waitKey(0);
   }
 
+  vector< Point3d > hlines_intersections; 
+  for (int i=0; i< int( hlines.size() ); i++)
+    for (int j=0; j<i; j++ ) // рассмотрим пары 0 <= j < i < hlines.size() 
+    {
+      Point3d i_j_intersection = hlines[i].cross( hlines[j] );
+      i_j_intersection = force_positive_direction( i_j_intersection );
+      hlines_intersections.push_back( i_j_intersection );
+    }
 
-  //////////// show hlines
+  UnionSpereAnglesRuler ruler( hlines_intersections );
 
-  for (int i=0; i< int(hlines.size()); i++)
+  //CoverNet< int, UnionSpereAnglesRuler >  cover_net( &ruler, CV_PI, 0.05 ); 
+  CoverNet< int, UnionSpereAnglesRuler >  cover_net( &ruler, CV_PI*16, 0.001 ); 
+  for (int i=0; i< hlines_intersections.size(); i++)
   {
-    cout << "hline: " << hlines[i] << endl;
-    cout << "length: " << length( hlines[i] ) << endl;
+    cover_net.insert(i);
   }
+
+  cover_net.reportStatistics();
+
+  dbgPressAnyKey();
+
+
 
   ////Metr1 ruler;  CoverTree< int, Metr1 > tree( &ruler, 1000, 1 );
   //Metr2 ruler;  CoverTree< int, Metr2 > tree( &ruler, 1000, 1 );
