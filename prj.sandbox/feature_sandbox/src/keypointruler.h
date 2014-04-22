e1b58e33c53fa2baababd7b1dcac20319f24c776
vicproon <vicproon@gmail.com>
2014-04-22 13:24:33 +0000

    [upd] stereo video FP (SURF) correspondence via CoverNet.

--- prj.sandbox/feature_sandbox/src/keypointruler.h
+++ prj.sandbox/feature_sandbox/src/keypointruler.h
@@ -20,7 +20,8 @@ public:
 
   double computeDistance(  const std::pair<SimpleFrame *, unsigned int> &pt1, const std::pair<SimpleFrame *, unsigned int> &pt2 )
   {
-    double alpha = 2.0 / 2202.0;
+    //double alpha = 2.0 / 2202.0; //mono 1920
+    double alpha = 2.0 / 1470.0; //stereo 1280
     cv::Mat desc1 = pt1.first->descriptors.row(pt1.second);
     cv::Mat desc2 = pt2.first->descriptors.row(pt2.second);
     cv::Point2f kp1 = pt1.first->kps[pt1.second].pt;
@@ -32,7 +33,8 @@ private:
   static double pointDist(const cv::Point2f &pt1, const cv::Point2f &pt2)
   {
     double res = sqrt(pow(pt1.x - pt2.x, 2.0) + pow(pt1.y - pt2.y, 2.0));
-    if (res <= 100)
+    //if (res <= 100)//mono 1920
+    if (res <= 200) //stereo 1280
       return 0;
     return res;
   }
--- prj.sandbox/feature_sandbox/src/main.cpp
+++ prj.sandbox/feature_sandbox/src/main.cpp
@@ -1,7 +1,8 @@
-﻿#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
-#include <opencv2\features2d\features2d.hpp>
-#include <opencv2\nonfree\nonfree.hpp>
+﻿#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -79,11 +80,23 @@ void Init()
 
 typedef CoverNet<std::pair<SimpleFrame *, unsigned int>, KeyPointDescriptorRuler> CNType;
 
-void TryCorrespond(Ptr<CNType> &curFrame, Ptr<SimpleFrame> &prevFrame);
+void InitCoverNet(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &pivotFrame);
+void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame);
+void TryCorrespondStereo(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &lFrame, Ptr<SimpleFrame> &rFrame);
+
+void MonoCorrespondTime();
+void StereoCorrespond();
 
 int main( int argc, char** argv )
 {
   Init();
+ // MonoCorrespondTime();
+  StereoCorrespond();
+  return 0;
+}
+
+void MonoCorrespondTime()
+{
   string ifilename = "/testdata/roadvideo/input/roadvideo.02/roadvideo.02.0001.avi";
 
   VideoCapture cap(ifilename);
@@ -96,7 +109,7 @@ int main( int argc, char** argv )
   double rootRadius = 5.0;
   double minRadius = 0.15;
   Ptr<CNType> coverNet = new CNType(&ruler, rootRadius, minRadius);
-  Ptr<SimpleFrame> prevFrame = NULL, curFrame = NULL;
+  Ptr<SimpleFrame> pivotFrame = NULL, curFrame = NULL;
   vector<Ptr<SimpleFrame> > lastFrames;
   lastFrames.reserve(10);
   int coverNetFlush = 0;
@@ -104,35 +117,36 @@ int main( int argc, char** argv )
   {
     char c = 0;
     if (!cap.read(bgr))
-      return 0;
+      return;
     if (!(iFrame % 4))
     {
       curFrame = new SimpleFrame(bgr, hcoords, iFrame);
       lastFrames.push_back(curFrame);
       curFrame->preprocess(featureDetector, descriptorExtractor);
-      //curFrame->draw();
-      TryCorrespond(coverNet, curFrame);
-      c = cvWaitKey(0);
-      prevFrame = curFrame;
-      ++coverNetFlush;
-      if (coverNetFlush == 10)
+      if (coverNetFlush == 0)
       {
         coverNet = new CNType(&ruler, rootRadius, minRadius);
+        pivotFrame = curFrame;
+        InitCoverNet(coverNet, pivotFrame);
         lastFrames.clear();
         lastFrames.reserve(10);
         coverNetFlush = 0;
       }
+      //curFrame->draw();
+      TryCorrespond(coverNet, curFrame);
+      c = cvWaitKey(2);
+      ++coverNetFlush;
+      coverNetFlush %= 10;
     }
     if (c == 27)
     {
-      return 0;
+      return;
     }
   }
 
-
-
-  return 0;
 }
+
+
 
 void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame)
 {
@@ -140,11 +154,17 @@ void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame)
   for (unsigned int i = 0; i < curFrame->kps.size(); ++i)
   {
     cv::circle(todraw, curFrame->kps[i].pt, 1, cv::Scalar(255,0,0), 2);
-    int iSph = coverNet->insert(make_pair(curFrame, i));
-    SimpleFrame *tmp = coverNet->getSphere(iSph).center.first;
-    int ikp = coverNet->getSphere(iSph).center.second;
-    cv::circle(todraw, tmp->kps[ikp].pt, 1, cv::Scalar(0,0,255), 1);
-    cv::line(todraw, curFrame->kps[i].pt, tmp->kps[ikp].pt, cv::Scalar(0,0,255), 1);
+    double dist = 0.2;
+    int iSph = coverNet->findNearestSphere(make_pair(curFrame, i), dist);
+    if (iSph == -1) //no newarest point found
+      iSph = coverNet->insert(make_pair(curFrame, i)); //creating new coverNet Sphere
+    if (iSph != -1)
+    {
+      SimpleFrame *tmp = coverNet->getSphere(iSph).center.first;
+      int ikp = coverNet->getSphere(iSph).center.second;
+      cv::circle(todraw, tmp->kps[ikp].pt, 1, cv::Scalar(0,0,255), 1);
+      cv::line(todraw, curFrame->kps[i].pt, tmp->kps[ikp].pt, cv::Scalar(0,0,255), 1);
+    }
   }
   cv::Mat rsz = todraw;
   //resize(todraw, rsz, Size(), 0.5, 0.5);
@@ -152,4 +172,91 @@ void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame)
   coverNet->reportStatistics();
   cout << endl << endl;
   return;
+}
+
+void StereoCorrespond()
+{
+  string filenameFormat = "/testdata/reika/input/reika.01/%s%04d.avi";
+  int iEpisode = 1;
+  
+  VideoCapture lcap(format(filenameFormat.c_str(), "left", iEpisode)), rcap(format(filenameFormat.c_str(), "right", iEpisode));
+  int nFrames = cvRound(lcap.get(CV_CAP_PROP_FRAME_COUNT));
+  int w = cvRound(lcap.get(CV_CAP_PROP_FRAME_WIDTH));
+  int h = cvRound(lcap.get(CV_CAP_PROP_FRAME_HEIGHT));
+  Mat lbgr;
+  Mat rbgr;
+  Ptr<HCoords> hcoords = new HCoords(w,h);
+  KeyPointDescriptorRuler ruler;
+  double rootRadius = 5.0;
+  double minRadius = 0.15;
+  Ptr<CNType> coverNet = new CNType(&ruler, rootRadius, minRadius);
+  Ptr<SimpleFrame> lFrame = NULL, rFrame = NULL;
+  vector<Ptr<SimpleFrame> > lastFrames;
+  lastFrames.reserve(10);
+  for (int iFrame = 0; iFrame < nFrames; ++iFrame)
+  {
+    char c = 0;
+    if (!lcap.read(lbgr) || !rcap.read(rbgr))
+      return;
+    lFrame = new SimpleFrame(lbgr, hcoords, iFrame);
+    rFrame = new SimpleFrame(rbgr, hcoords, iFrame);
+    lFrame->preprocess(featureDetector, descriptorExtractor);
+    rFrame->preprocess(featureDetector, descriptorExtractor);
+    coverNet = new CNType(&ruler, rootRadius, minRadius);
+    InitCoverNet(coverNet, lFrame);
+    TryCorrespondStereo(coverNet, lFrame, rFrame);
+    c = cvWaitKey(1);
+    if (c == 27)
+    {
+      return;
+    }
+  }
+
+}
+
+
+void TryCorrespondStereo(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &lFrame, Ptr<SimpleFrame> &rFrame)
+{
+  Size sz = lFrame->src.size();
+  cv::Mat todraw = Mat::zeros(Size(sz.width * 2, sz.height), CV_8UC3);
+  lFrame->src.copyTo(todraw(Rect(Point(0,0), sz)));
+  rFrame->src.copyTo(todraw(Rect(Point(sz.width,0), sz)));
+  for (unsigned int i = 0; i < lFrame->kps.size(); ++i)
+  {
+    cv::circle(todraw, lFrame->kps[i].pt, 1, cv::Scalar(255,0,0), 2);
+  }
+  for (unsigned int i = 0; i < rFrame->kps.size(); ++i)
+  {
+    Point pt = rFrame->kps[i].pt;
+    pt.x += lFrame->src.cols;
+    cv::circle(todraw, pt, 1, cv::Scalar(255,0,0), 2);
+    double dist = 0.2;
+    int iSph = coverNet->findNearestSphere(make_pair(rFrame, i), dist);
+    /*
+    if (iSph == -1) //no newarest point found
+      iSph = coverNet->insert(make_pair(rFrame, i)); //creating new coverNet Sphere
+      */
+    if (iSph != -1)
+    {
+      SimpleFrame *tmp = coverNet->getSphere(iSph).center.first;
+      int ikp = coverNet->getSphere(iSph).center.second;
+      cv::line(todraw, pt, tmp->kps[ikp].pt, cv::Scalar(0,0,255), 2);
+    }
+  }
+  cv::Mat rsz;
+  resize(todraw, rsz, Size(), 1400.0 / todraw.cols, 1400.0 / todraw.cols);
+  imshow("cvnet", rsz);
+  coverNet->reportStatistics();
+  cout << endl << endl;
+  return;
+}
+
+void InitCoverNet(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &pivotFrame)
+{
+  if (!coverNet || !pivotFrame)
+    return;
+  for (unsigned int i = 0; i < pivotFrame->kps.size(); ++i)
+  {
+    coverNet->insert(make_pair(pivotFrame, i));
+  }
 }
