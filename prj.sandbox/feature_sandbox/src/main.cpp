d19656fd9f484e8addd03c06f605c677a5bc931e
vicproon <vicproon@gmail.com>
2014-04-23 13:15:14 +0000

    [upd] Dumbbell rotation vectors CoverNet.

--- prj.sandbox/feature_sandbox/src/main.cpp
+++ prj.sandbox/feature_sandbox/src/main.cpp
@@ -10,7 +10,7 @@
 #include "simpleframe.h"
 #include "keypointruler.h"
 
-#include <ocvutils\hcoords.h> 
+#include <ocvutils/hcoords.h> 
 #include <cover_net/cover_net.h>
 
 
@@ -85,13 +85,15 @@ void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame);
 void TryCorrespondStereo(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &lFrame, Ptr<SimpleFrame> &rFrame);
 
 void MonoCorrespondTime();
+void MonoCorrespondDumbbells();
 void StereoCorrespond();
 
 int main( int argc, char** argv )
 {
   Init();
  // MonoCorrespondTime();
-  StereoCorrespond();
+  MonoCorrespondDumbbells();
+ // StereoCorrespond();
   return 0;
 }
 
@@ -151,6 +153,7 @@ void MonoCorrespondTime()
 void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame)
 {
   cv::Mat todraw = curFrame->src.clone();
+  
   for (unsigned int i = 0; i < curFrame->kps.size(); ++i)
   {
     cv::circle(todraw, curFrame->kps[i].pt, 1, cv::Scalar(255,0,0), 2);
@@ -162,7 +165,7 @@ void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame)
     {
       SimpleFrame *tmp = coverNet->getSphere(iSph).center.first;
       int ikp = coverNet->getSphere(iSph).center.second;
-      cv::circle(todraw, tmp->kps[ikp].pt, 1, cv::Scalar(0,0,255), 1);
+      cv::circle(todraw, tmp->kps[ikp].pt, 1, cv::Scalar(0,0,255), 1);  
       cv::line(todraw, curFrame->kps[i].pt, tmp->kps[ikp].pt, cv::Scalar(0,0,255), 1);
     }
   }
@@ -171,6 +174,7 @@ void TryCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &curFrame)
   imshow("cvnet", rsz);
   coverNet->reportStatistics();
   cout << endl << endl;
+  
   return;
 }
 
@@ -260,3 +264,154 @@ void InitCoverNet(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &pivotFrame)
     coverNet->insert(make_pair(pivotFrame, i));
   }
 }
+
+void DumbbellCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &pivotFrame, Ptr<SimpleFrame> &curFrame);
+
+void MonoCorrespondDumbbells()
+{
+  string ifilename = "/testdata/roadvideo/input/roadvideo.02/roadvideo.02.0001.avi";
+  VideoCapture cap(ifilename);
+  int nFrames = cvRound(cap.get(CV_CAP_PROP_FRAME_COUNT));
+  int w = cvRound(cap.get(CV_CAP_PROP_FRAME_WIDTH));
+  int h = cvRound(cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+  Mat bgr;
+  Ptr<HCoords> hcoords = new HCoords(w,h);
+  KeyPointDescriptorRuler ruler;
+  double rootRadius = 5.0;
+  double minRadius = 0.15;
+  Ptr<CNType> coverNet = new CNType(&ruler, rootRadius, minRadius);
+  Ptr<SimpleFrame> pivotFrame = NULL, curFrame = NULL;
+  int coverNetFlush = 0;
+  for (int iFrame = 0; iFrame < nFrames; ++iFrame)
+  {
+    char c = 0;
+    if (!cap.read(bgr))
+      return;
+    if (!(iFrame % 4))
+    {
+      curFrame = new SimpleFrame(bgr, hcoords, iFrame);
+      curFrame->preprocess(featureDetector, descriptorExtractor);
+      if (coverNetFlush == 0)
+      {
+        coverNet = new CNType(&ruler, rootRadius, minRadius);
+        pivotFrame = curFrame;
+        InitCoverNet(coverNet, pivotFrame);
+        coverNetFlush = 0;
+      }
+      DumbbellCorrespond(coverNet, pivotFrame, curFrame);
+      curFrame->draw();
+      c = cvWaitKey(0);
+      ++coverNetFlush;
+      coverNetFlush %= 10;
+    }
+    if (c == 27)
+    {
+      return;
+    }
+  }
+}
+
+struct Dumbbell
+{
+  SimpleFrame *curFrame;
+  SimpleFrame *pivotFrame;
+  vector<pair<unsigned int, unsigned int>> &matches; // pairs: <curFrame fp index, pivotFrame fp index>
+  std::pair<unsigned int, unsigned int> heads; //indices of dumbbell heads entries in matches array
+
+  Point3d rotationVector;
+
+  Dumbbell(vector<pair<unsigned int, unsigned int>> &matches_)
+    :matches(matches_) {}
+
+  Point3d & ComputeRotationVector()
+  {
+    HPoint3d P1 = pivotFrame->sphere_points[matches[heads.first].second];
+    HPoint3d P2 = pivotFrame->sphere_points[matches[heads.second].second];
+    HPoint3d Q1 = curFrame->sphere_points[matches[heads.first].first];
+    HPoint3d Q2 = curFrame->sphere_points[matches[heads.second].first];
+
+    Point3d d1 = Q1 - P1;
+    Point3d d2 = Q2 - P2;
+
+    double cosd1d2 = d1.dot(d2) / sqrt((d1.dot(d1) * d2.dot(d2)));
+    Point3d n = (d1).cross(d2);
+    if (cosd1d2 >= 0.99)
+    {
+      cout << "dumbbell points lie on 1 meridian" << endl;
+      n = P1.cross(P2).cross(Q1.cross(Q2));
+    }
+    double norm_n = norm(n);
+    if (norm_n != 0)
+    {
+      n.x /= norm_n;
+      n.y /= norm_n;
+      n.z /= norm_n;
+    }
+    rotationVector = n;
+    return rotationVector;
+  }
+};
+
+
+
+
+void DumbbellCorrespond(Ptr<CNType> &coverNet, Ptr<SimpleFrame> &pivotFrame, Ptr<SimpleFrame> &curFrame)
+{  
+  if (pivotFrame == curFrame)
+    return;
+  //find matches
+  vector<pair<unsigned int, unsigned int>> matches;
+  for (unsigned int i = 0; i < curFrame->kps.size(); ++i)
+  {
+    double dist = 0.2;
+    int iSph = coverNet->findNearestSphere(make_pair(curFrame, i), dist);
+    if (iSph != -1)
+    {
+      int ikp = coverNet->getSphere(iSph).center.second;
+      matches.push_back(make_pair(i, coverNet->getSphere(iSph).center.second));
+    }
+  }
+  
+  //for all matches make a Dumbbell
+  vector<Dumbbell> dumbbells;
+  for (int i = 0; i < matches.size(); ++i)
+    for (int j = i + 1; j < matches.size(); ++j)
+    {
+      Dumbbell d(matches);
+      d.curFrame = curFrame;
+      d.pivotFrame = pivotFrame;
+      d.heads.first = i;
+      d.heads.second = j;
+      dumbbells.push_back(d);
+    }
+
+  UnitSphereAnglesRuler angRuler;
+  CoverNet<Point3d &, UnitSphereAnglesRuler> angCNet(&angRuler, CV_PI, CV_PI/180);
+  for (int i = 0; i < dumbbells.size(); ++i)
+  {
+    angCNet.insert(dumbbells[i].ComputeRotationVector());
+  }
+
+  angCNet.reportStatistics();
+  return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
