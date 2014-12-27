8392744ed241534b6de49fac94a386e59d9e966c
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2014-12-27 00:08:47 +0000

    markup: добавлен запрос координат к NMEA, сделана подгрузка карт-листов

--- trunk/prj.core/geomap/geomap.cpp
+++ trunk/prj.core/geomap/geomap.cpp
@@ -2,9 +2,12 @@
 #include "geomap/geomap.h"
 
 
-bool GeoMap::open(  const char* image_file  )
+bool GeoSheet::read( const char* sheet_file_name )
 {
-  raster = imread(image_file);
+  raster = imread( sheet_file_name ); // todo try { }
+  if (raster.empty()) 
+    return false;
+
   //imshow("GeoMap", raster);
   //waitKey(0);
 
@@ -13,4 +16,29 @@ bool GeoMap::open(  const char* image_file  )
   
   return true;
 }	
+
+bool GeoMap::open( const char *sheets_list_file )
+{
+  string root(sheets_list_file);
+  ifstream ifs( root.c_str() );
+  if (!ifs.is_open())
+    return false;
+  int nsheets = 0;
+  ifs >> nsheets;
+  if (nsheets <=0 || nsheets > 1024)
+    return false;
+  sheets.resize(nsheets);
+  for ( int i=0; i<nsheets; i++ )
+  {
+    GeoSheet& sh = sheets[i]; 
+    string sheet_name;
+    ifs >> sheet_name;
+    string sheet_path = root + "/../" + sheet_name;
+    if ( !sh.read( sheet_path.c_str() ))
+      return false;
+    ifs >> sh.a.xy.x >> sh.a.xy.y >> sh.a.ns.y >> sh.a.ns.x; 
+    ifs >> sh.b.xy.x >> sh.b.xy.y >> sh.b.ns.y >> sh.b.ns.x; 
+  }
+  return true;
+}
 
