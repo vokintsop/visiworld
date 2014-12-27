a5f636e15b32f5ac3af929ce3bfc6d6cabab01bf
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2014-12-27 00:54:12 +0000

    markup: добавлено параллельное с движением отображение на карте

--- trunk/prj.core/geomap/geomap.cpp
+++ trunk/prj.core/geomap/geomap.cpp
@@ -2,6 +2,7 @@
 #include "geomap/geomap.h"
 
 
+
 bool GeoSheet::read( const char* sheet_file_name )
 {
   raster = imread( sheet_file_name ); // todo try { }
@@ -40,5 +41,35 @@ bool GeoMap::open( const char *sheets_list_file )
     ifs >> sh.b.xy.x >> sh.b.xy.y >> sh.b.ns.y >> sh.b.ns.x; 
   }
   return true;
+}
+
+void GeoMap::draw( Point2d en )
+{
+  double best_dd = 1.;
+  int best_i=-1;
+  for (int i=0; i<sheets.size(); i++)
+  {
+    GeoSheet& sh = sheets[i];
+    Point xy = sh.ns2xy( en );
+    double dx = abs( double( sh.raster.cols/2 - xy.x ) / sh.raster.cols );
+    double dy = abs( double( sh.raster.rows/2 - xy.y ) / sh.raster.rows );
+
+    if (dx > 0.5 || dy > 0.5)
+      continue;
+    
+    double dd = max( dx, dy );
+    if (dd < best_dd)
+    {
+      best_dd = dd;
+      best_i = i;
+    }
+  }
+  if (best_i != -1)
+  {
+    Mat draw = sheets[best_i].raster.clone();
+    circle( draw, sheets[best_i].ns2xy( en ), 3, Scalar( 0, 0, 255 ) );
+    imshow( "GeoMap111", draw );
+    //waitKey(1);
+  }
 }
 
