c1cef35807cd0a631145376e6c4def6af90ce769
khammatova.diana@gmail.com <khammatova.diana@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2015-01-13 12:33:00 +0000

    в fht.h добавлен #include <opencv2/imgproc/imgproc.hpp> для line и в main.cpp #include <opencv2/imgproc/imgproc.hpp> для  cvtColor

--- trunk/prj.sandbox/fht/src/fht.h
+++ trunk/prj.sandbox/fht/src/fht.h
@@ -5,5 +5,6 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 void fht_vertical(cv::Mat1b &input, cv::Mat1i &outputl, cv::Mat1i &outputr);
