62fdc0b01cc38562a543d74efde4c2353eb90fc7
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2015-01-21 02:51:41 +0000

    markup: вынес основные части в mainframe

--- trunk/prj.sandbox/markup/src/mainframe.h
+++ trunk/prj.sandbox/markup/src/mainframe.h
@@ -1,5 +1,16 @@
 #ifndef __MAINFRAME_H
 #define __MAINFRAME_H
 
+#include "geomap/geomapeditor.h"
+#include "markup/markupeditor.h"
+#include "maptorealworld.h"
+
+class MarkupMainFrame
+{
+public:
+  Ptr< GeoMapEditor >  pGeoMapEditor; // singleton
+  Ptr< MarkupEditor >  pMarkupEditor;
+  Ptr< Camera2DPoseEstimator> pCamPoseEst;
+};
 
 #endif // __MAINFRAME_H
