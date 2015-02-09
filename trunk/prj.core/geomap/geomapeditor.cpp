e1f490a9521efe46ca7a55edad3ad637f4842d88
aliev.michael@gmail.com <aliev.michael@gmail.com@0076c2d9-860a-a353-ee00-78913fff4c9e>
2015-02-09 16:32:16 +0000

    [upd/add] geomap: мелкие правки; geomapeditor: много временного копи-паста с оставлением только текуще-нужного; mainframe: проверка окна; markup: мелкие правки; g-storage: новый модуль, примитивный черновик.

--- trunk/prj.core/geomap/geomapeditor.cpp
+++ trunk/prj.core/geomap/geomapeditor.cpp
@@ -12,7 +12,7 @@ void mouseCallBack4GeoMapEditor(int event, int x, int y, int flags, void *userda
 }
 
 GeoMapEditor::GeoMapEditor( const char* _root_folder ) /// = NULL )
-  : iObjType(0), title(GEOMAP_EDITOR_TITLE), rubbering_mode(0)
+  : iObjType(0), title(GEOMAP_EDITOR_TITLE), rubbering_mode(0)//, non_stop_mode(false)
 { 
   namedWindow( title, WINDOW_NORMAL ); //WINDOW_AUTOSIZE); // -- в режиме AUTOSIZE координаты x y мышки надо пересчитывать
   setMouseCallback( title, mouseCallBack4GeoMapEditor, this);
@@ -77,11 +77,13 @@ void GeoMapEditor::draw()
     for (int i=0; i< int(gm.objects.size()); i++)
     {
       AGMObject* pagmo = gm.objects[i];
-      for (int j=0; j< int(pagmo->pts.size()); j++)
-      {
-        Point pt = gm.sheets[cur_sheet].en2xy( pagmo->pts[j] );
-        circle( draw, pt, 5, Scalar( 255, 0, 255 ), 2 );
-      }
+      pagmo->draw(sh, draw);
+
+//       for (int j=0; j< int(pagmo->pts.size()); j++)
+//       {
+//         Point pt = gm.sheets[cur_sheet].en2xy( pagmo->pts[j] );
+//         circle( draw, pt, 5, Scalar( 255, 0, 255 ), 2 );
+//       }
     }
     imshow( title, draw );
     //waitKey(1);
@@ -114,10 +116,33 @@ int GeoMapEditor::finishMouseEvent() // евент обработан, надо отрисовать и закон
 
 #define ADD_OBJECT_TRACKED      0x1000 // автоматическое добавление в режиме трекинга
 
+// void GeoMapEditor::drawRubbering()
+// {
+//   if ( ! (cur_sheet >= 0 && cur_sheet < int(gm.sheets.size()) ) )
+//     return;
+//   
+//   GeoSheet& sh =  gm.sheets[cur_sheet];
+//   Mat draw_image = sh.raster.clone();
+// 
+//   if (rubbering_mode == RUBBERING_SEGMENT || rubbering_mode == RUBBERING_POLYGON) // надо нарисовать предыдущие точки и ребра
+//     for (int i=0; i< int(rubbering_pts.size()); i++)
+//     {
+//       circle( draw_image, rubbering_pts[i], 5, Scalar( 255, 255, 0 ) );
+//       if (i>0)
+//         line( draw_image, rubbering_pts[i], rubbering_pts[i-1], Scalar( 255, 255, 0 ) );
+//       else if (rubbering_pts.size() >2)
+//         line( draw_image, rubbering_pts[0], rubbering_pts.back(), Scalar( 255, 255, 0 ) );
+//     }
+// 
+//     if (rubbering_mode == RUBBERING_RECTANGLE && rubbering_pts.size() == 2)
+//     {
+//       Rect rect( rubbering_pts[0], rubbering_pts[1] );
+//       rectangle( draw_image, rect, Scalar(0,250,0), 2 );
+//     }
+// }
 
 int GeoMapEditor::processMouseEvent(int event, int x, int y, int flags)
 {
-
   //switch (mouseScenario)
   //{
 
@@ -126,16 +151,12 @@ int GeoMapEditor::processMouseEvent(int event, int x, int y, int flags)
   //cout << "event=" << event << " coords=" << x << " " << y << " flags=" << flags << endl;
 
   string objtype = objType(); // тип объекта, с которым работаем
-  bool object_poligonal = (objtype == "AFO_Segm" || objtype == "AFO_Triangle" || objtype == "AFO_Quad" || objtype == "AFO_Polygon");
+  bool object_poligonal = (objtype == "AGM_Segm" );
   ///bool object_segment_based = (objtype == "AFO_Segm"); // либо производные двух точечные отрезкообразные
   cv::Point curPoint(x,y);
   int max_poly_points=1000;
-  if (objtype == "AFO_Segm")
+  if (objtype == "AGM_Segm")
     max_poly_points=2;
-  if (objtype == "AFO_Triangle")
-    max_poly_points=3;
-  if (objtype == "AFO_Quad")
-    max_poly_points=4;
 
   //cout << "GeoMap:objtype" << objtype << endl;
 
@@ -151,8 +172,9 @@ int GeoMapEditor::processMouseEvent(int event, int x, int y, int flags)
           rubbering_pts.push_back(curPoint); // создали вырожденный первый сегмент, вторую точку будем корректировать на mousemove 
           rubbering_mode = RUBBERING_SEGMENT;
         }
-        else if (rubbering_pts.size()>=2 && // если последний сегмент не вырожденный -- начнем новый
-          l2norm( rubbering_pts[rubbering_pts.size()-2], rubbering_pts[rubbering_pts.size()-1] ) >= 25  )
+        else
+//           if (rubbering_pts.size()>=2 && // если последний сегмент не вырожденный -- начнем новый
+//           l2norm( rubbering_pts[rubbering_pts.size()-2], rubbering_pts[rubbering_pts.size()-1] ) >= 25  )
         {
           rubbering_pts.push_back(curPoint);
           rubbering_mode = RUBBERING_SEGMENT;
@@ -169,6 +191,8 @@ int GeoMapEditor::processMouseEvent(int event, int x, int y, int flags)
       return finishMouseEvent();
 
     case CV_EVENT_MOUSEMOVE     : // =0,
+      if (rubbering_mode == RUBBERING_NOTHING)
+        break;
       if (rubbering_mode == RUBBERING_RECTANGLE)
       {
         assert(rubbering_pts.size() == 2);
@@ -182,19 +206,21 @@ int GeoMapEditor::processMouseEvent(int event, int x, int y, int flags)
           rubbering_pts.back() = curPoint; // обновили точку
         return finishMouseEvent();
       }
-      if (rubbering_mode == RUBBERING_POLYGON) // уже вытянута часть многоугольника, и мы свободно водим мышкой планируя начать вытягивать следующее ребро многоугольника
-      {
-        assert(rubbering_pts.size() >=2);
-        // ничего не делаем, пока выбор пользователя не понятен и отрисовка не меняется
-        //return 0;
-        rubbering_pts.back() = curPoint;
-        return finishMouseEvent();
-      }
+//       if (rubbering_mode == RUBBERING_POLYGON) // уже вытянута часть многоугольника, и мы свободно водим мышкой планируя начать вытягивать следующее ребро многоугольника
+//       {
+//         assert(rubbering_pts.size() >=2);
+//         // ничего не делаем, пока выбор пользователя не понятен и отрисовка не меняется
+//         //return 0;
+//         rubbering_pts.back() = curPoint;
+//         return finishMouseEvent();
+//       }
 
     //case CV_EVENT_RBUTTONDOWN   : // =2,
     //case CV_EVENT_MBUTTONDOWN   : // =3,
     case CV_EVENT_LBUTTONUP     : // =4,
     case CV_EVENT_RBUTTONUP     : // =4,
+      if (rubbering_mode == RUBBERING_NOTHING)
+        break;
       if (rubbering_mode == RUBBERING_RECTANGLE) 
       {
         assert(rubbering_pts.size() == 2);
@@ -212,30 +238,35 @@ int GeoMapEditor::processMouseEvent(int event, int x, int y, int flags)
       {
         assert(rubbering_pts.size() >= 2);
         rubbering_pts.back() = curPoint;
-        if ( l2norm( rubbering_pts[0], curPoint) < 100 // замкнули или просто клик был. конец банкета
-          //////||
-          //////  (object_segment_based && rubbering_pts.size() == 2 )
-          || rubbering_pts.size() == max_poly_points+1
-            ) 
-        {
-          if (rubbering_pts.size() > 2)
-            rubbering_pts.back() = rubbering_pts[0]; // скорректировали последнюю точку прилепив к начальной
-          int flags = ADD_OBJECT_RECT | (rubber_by_left_button ? ADD_OBJECT_MOUSE_LEFT : ADD_OBJECT_MOUSE_RIGHT);
-          if (l2norm( rubbering_pts[rubbering_pts.size()-2], rubbering_pts[rubbering_pts.size()-1] ) < 25)
-          { // удаляем последнее ребро если оно вырожденное
-            rubbering_pts[rubbering_pts.size()-2] = rubbering_pts[0];
-            rubbering_pts.pop_back();
-          }
-          rubbering_mode = RUBBERING_NOTHING;
-          int res = addMouseObject(rubbering_pts, flags);
-          rubbering_pts.clear();
-          return res;
-        }
+//         if ( l2norm( rubbering_pts[0], curPoint) < 100 // замкнули или просто клик был. конец банкета
+//           //////||
+//           //////  (object_segment_based && rubbering_pts.size() == 2 )
+//           || rubbering_pts.size() == max_poly_points+1
+//             ) 
+//         {
+//           if (rubbering_pts.size() > 2)
+//             rubbering_pts.back() = rubbering_pts[0]; // скорректировали последнюю точку прилепив к начальной
+//           int flags = ADD_OBJECT_RECT | (rubber_by_left_button ? ADD_OBJECT_MOUSE_LEFT : ADD_OBJECT_MOUSE_RIGHT);
+//           if (l2norm( rubbering_pts[rubbering_pts.size()-2], rubbering_pts[rubbering_pts.size()-1] ) < 25)
+//           { // удаляем последнее ребро если оно вырожденное
+//             rubbering_pts[rubbering_pts.size()-2] = rubbering_pts[0];
+//             rubbering_pts.pop_back();
+//           }
+//           rubbering_mode = RUBBERING_NOTHING;
+//           int res = addMouseObject(rubbering_pts, flags);
+//           rubbering_pts.clear();
+//           return res;
+//         }        
+        rubbering_mode = RUBBERING_NOTHING;
+        int res = addMouseObject(rubbering_pts, flags);
+        rubbering_pts.clear();
+        return res;
+//         }
         // не замкнулись, продолжаем, надо перерисовать
-        rubbering_pts.back() = curPoint;
-        rubbering_mode = RUBBERING_POLYGON;
+//         rubbering_pts.back() = curPoint;
+//         rubbering_mode = RUBBERING_POLYGON;
 
-        return finishMouseEvent();
+//        return finishMouseEvent();
       }
       break;
     //case CV_EVENT_RBUTTONUP     : // =5,
@@ -251,11 +282,27 @@ bool GeoMapEditor::addMouseObject( // пытаемся добавить новый объект вытянув или
     cv::Rect& rect, // note: in-out -- подкручиваем ректангл по законам первого рождения для данного объекта
     int flags )
 {
-  Point xy = center( rect );
-  GeoSheet& sh = gm.sheets[ cur_sheet ];
-  Point2d en = sh.xy2en( xy );
-  AGM_Point* pp = new AGM_Point( en );
-  gm.objects.push_back(cv::Ptr<AGM_Point>(pp));
+  
+  if (objType() == "AGM_Segm")
+  {
+    Point xyTL = rect.tl();
+    Point xyBR = rect.br();
+    GeoSheet& sh = gm.sheets[ cur_sheet ];
+    Point2d enTL = sh.xy2en( xyTL );
+    Point2d enBR = sh.xy2en( xyBR );
+
+    AGM_Segm* ps = new AGM_Segm(enTL, enBR);
+    gm.objects.push_back(cv::Ptr<AGM_Segm>(ps));
+  }
+  else
+  {
+    Point xy = center( rect );
+    GeoSheet& sh = gm.sheets[ cur_sheet ];
+    Point2d en = sh.xy2en( xy );
+    AGM_Point* pp = new AGM_Point( en );
+    gm.objects.push_back(cv::Ptr<AGM_Point>(pp));
+  }
+
   return true;
 };
 
@@ -264,5 +311,135 @@ bool GeoMapEditor::addMouseObject( // пытаемся добавить новый объект вытянув или
   std::vector< cv::Point >& pts, // note: in-out -- подкручиваем точки по законам первого рождения для данного объекта
   int flags )
 {
+  if (objType() == "AGM_Segm" && pts.size() > 1)
+  {
+    Point xyStart = pts[0];
+    Point xyEnd = pts[1];
+    GeoSheet& sh = gm.sheets[ cur_sheet ];
+    Point2d enStart = sh.xy2en( xyStart );
+    Point2d enEnd = sh.xy2en( xyEnd );
+
+    AGM_Segm* ps = new AGM_Segm(enStart, enEnd);
+    gm.objects.push_back(cv::Ptr<AGM_Segm>(ps));
+    return true;
+  }
+
   return __false("GeoMapEditor::addMouseObject(std::vector< cv::Point >& pts) not implemented");
 };
+
+int GeoMapEditor::processEvents()
+{
+  update_window();
+  return processKeys();
+}
+
+int GeoMapEditor::processKeys() // здесь обработка клавиш до выхода из цикла обработки данного кадра 
+{
+  //while (1)
+  {
+    int key = kNoKeyPressed;
+    if (pressed_keys.size() > 0)
+    {
+      key = pressed_keys.front(); // пока не извлекаем...
+    }
+    // здесь обработка клавиш до выхода из цикла обработки данного кадра 
+
+    if (key == kBackSpace)
+    {
+      pressed_keys.pop_front();
+      if (gm.objects.size() > 0)
+      {
+        gm.objects.pop_back();
+      }
+//       FrameData& frame_data = marked_frames[iframe];
+//       if (frame_data.objects.size()>0)
+//       {
+//         deleteFrameObjectImage(frame_data.objects.size()-1);
+//         frame_data.objects.pop_back();
+//       }
+      update_image_to_draw();
+      //draw();
+      update_window();
+      //continue;
+    }
+//     if (key == kF1)
+//     {
+//       pressed_keys.pop_front();
+//       help();
+//       continue;
+//     }
+//     if (key == kF2)
+//     {
+//       pressed_keys.pop_front();
+//       saveMarkupData();
+//       continue;
+//     }
+//     if (key == kF3)
+//     {
+//       pressed_keys.pop_front();
+//       saveFrameImage();
+//       continue;
+//     }
+//     if (key == kF4)
+//     {
+//       pressed_keys.pop_front();
+//       saveFrameObjectsImages();
+//       continue;
+//     }
+    if (key == kTab)
+    {
+      pressed_keys.pop_front();
+      iObjType = (iObjType+1)% agmTypes.objTypes.size();
+      cout << "G size " << pressed_keys.size() << endl;
+      update_image_to_draw();
+      update_window();
+      //continue;
+    }
+// 
+//     if (key == kEnter || key == kCtrlEnter 
+//       || (key == kSpace && tracking_object) 
+//       ) // start-stop tracking
+//     {
+//       pressed_keys.pop_front();
+//       if (tracking_object) // stop tracking
+//       {
+//         tracking_object = false;
+//         non_stop_mode = false;
+//         update_image_to_draw();
+//         update_window();
+//         continue;
+//       }
+// 
+//       if (!non_stop_mode && !tracking_object) // start tracking
+//       { // start tracking mode
+//         if (key == kEnter)
+//           track_forward = true;
+//         else if (key == kCtrlEnter)
+//           track_forward = false;
+//         else
+//           assert(0); //третьего не дано
+// 
+//         FrameData& frame_data = marked_frames[iframe];
+//         if (frame_data.objects.size() > 0)
+//         {
+//           AFrameObject* afo = frame_data.objects.back(); // или взять последний добавленный в undo() ?
+//           non_stop_mode = true;
+//           tracked_rect = afo->rect;
+//           tracking_object = true;
+//           cout << "Start tracking " << afo->type << afo->rect << endl;
+//         }
+// 
+//         //vector< Rect > track_result;
+//         //trackRectangle( video, afo->rect, 20, track_result );
+//         // тут надо выгружать результаты track_result, 
+//         // но пока не дотянут файл разметки до MarkupEditor.
+//         // ...
+//         update_image_to_draw();
+//         //??update_window();
+//         continue;
+//       }
+//     } // (key == kEnter)
+    return key; // неизвестная клавиша -- снаружи разберутся
+  } // while (1)
+  return kNoKeyPressed;
+}
--- trunk/prj.core/mainframe/mainframe.cpp
+++ trunk/prj.core/mainframe/mainframe.cpp
@@ -6,12 +6,34 @@ MarkupMainFrame theFrame;
 
 int MarkupMainFrame::waitKey( int delay )
 {
-  // select active window
+  int key = cv::waitKey(delay);
+
+  if (key == kNoKeyPressed)
+    return key;
+
+  HWND hWnd = GetActiveWindow();
+
+  char lpBuff[MAX_PATH] = {0};
+  int ret = GetWindowText(hWnd, lpBuff, MAX_PATH);
+
+  std::string strWindowTitle(lpBuff);
+  std::string strAGM = "type=AGM_";
+  int nf = strWindowTitle.find(strAGM);
+  if (std::string::npos != nf)
+  {
+    this->pGeoMapEditor->pressed_keys.push_back(key);
+    this->pGeoMapEditor->processKeys();
+    if (key == kTab || key == kBackSpace)
+    {
+      key = kNoKeyPressed;
+    }
+  }
+  // select active window;
   //...
   // if none -- ask others
   //...
 
-  return cv::waitKey(delay);
+  return key;//cv::waitKey(delay);
 }
 
 int WaitKey( int delay )// централизованный обработчик cv::waitKey(), копит клавиши в буфере, распределяет клавиши для обработки между окнами
